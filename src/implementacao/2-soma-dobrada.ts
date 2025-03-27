bc38409bcf8bb7aab4baee6aa6c238ce74ce81ef
GessicaGobbi <gessicagobbi@hotmail.com>
2025-03-27 11:13:55 -0300

    exercicio 1até o 4

--- src/implementacao/2-soma-dobrada.ts
+++ src/implementacao/2-soma-dobrada.ts
@@ -1,4 +1,10 @@
 function somaDobrada(umValor: number, outroValor: number) {
-    return null;
+   if (umValor==outroValor) {
+        return ((umValor+outroValor)+(umValor+outroValor));
+   } else if(umValor<1 || outroValor<1){
+        return -1;
+    
+   }
+    return (umValor+outroValor);
 }
 module.exports = somaDobrada;
--- src/implementacao/3-fizz-buzz.ts
+++ src/implementacao/3-fizz-buzz.ts
@@ -1,4 +1,13 @@
 function fizzBuzz(numero: number) {
-    return null;
+    if (numero%5==0 && numero%3==0) {
+        return "FizzBuzz";
+    } else if(numero%5==0) {
+        return "Buzz";
+        
+    } else if(numero%3==0) {
+        return "Fizz";
+        
+    } 
+    else return (numero).toString();
 }
 module.exports = fizzBuzz;
